b3425e5be97555e48f0e6d7344689de7a9739eb0
Tony Schock <tony.schock@gmail.com>
2023-08-19 18:17:19 -0600

    Iterative Binary search

--- AlgoPython/main.py
+++ AlgoPython/main.py
@@ -58,3 +58,42 @@
 #     board = [-1] * n
 #     return n_queens_helper(board, 0)
 # print(n_queens(4))
+
+# Linear search
+
+# def search(arr, target):
+#     for i in range(len(arr)):
+
+#         if arr [i] == target:
+#             return i
+        
+# arr = [2,5,8,9,10,16,22]
+# target = 10
+
+# print(search(arr, target))
+
+# Iterative Binary search
+
+def binary_itr(arr, start, end, target):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] < target:
+            start = mid + 1
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            return mid
+    return -1
+    # return start
+
+arr = [2,5,8,10,16,22,25]
+target = 10
+
+result = binary_itr(arr, 0, len(arr)-1, target)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+    
+# @33:47
